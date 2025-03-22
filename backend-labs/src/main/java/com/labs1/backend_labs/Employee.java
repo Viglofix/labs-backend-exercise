93cb52e606c85c9144234bbf3c6f17c5d2b339cf
= <=>
2025-03-22 23:28:01 +0100

    InitialCommit

---
+++ backend-labs/src/main/java/com/labs1/backend_labs/Employee.java
@@ -0,0 +1,23 @@
+package com.labs1.backend_labs;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "job_name",nullable = false,unique = true)
+    private String jobName;
+}
---
+++ backend-labs/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee(job_name) VALUES ('Dev-ops');
+INSERT INTO employee(job_name) VALUES ('PM');
+INSERT INTO employee(job_name) VALUES ('Backend');
